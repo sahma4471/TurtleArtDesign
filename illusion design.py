ac1e1b74e6d70bc58dbe9e724a45481329e1addf
sahma4471 <33323168+sahma4471@users.noreply.github.com>
2017-11-03 11:05:52 -0400

    Update illusion design.py

--- illusion design.py
+++ illusion design.py
@@ -1,14 +1,19 @@
 import turtle
-from Jump import * # Imports every function from Jump.py 
+from Jump import * 
+# Imports every function from Jump.py 
 illusion = turtle.Turtle()
 
-illusion.speed(0) # sets the speed so it can be fast, 0 is the fastest.
-turtle.bgcolor('black') # sets the background color to black
+illusion.speed(0) 
+# sets the speed so it can be fast, 0 is the fastest.
+turtle.bgcolor('black') 
+# sets the background color to black
 illusion.color("cyan")
 
 
-for times in range(200): # repeats the code certain amount of times.
-    illusion.forward(400) # going toward and setting the size
+for times in range(200): 
+    # repeats the code certain amount of times.
+    illusion.forward(400) 
+    # going toward and setting the size
     illusion.right(30)
     illusion.forward(20)
     illusion.left(60)
@@ -18,7 +23,6 @@ for times in range(200): # repeats the code certain amount of times.
     jump (illusion, 0, 0)
     
     illusion.right(2)
-turtle.done() # adds some finishes touhes to the design
    
     
     
